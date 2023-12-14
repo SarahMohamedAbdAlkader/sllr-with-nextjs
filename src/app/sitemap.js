7e815d84106ded25dc0d212aaa54490622804b68
Sarah Mohamed <sarah.abdalkader@bosta.co>
2023-12-14 12:19:06 +0200

    Add sitemap for the whole app

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -12,8 +12,9 @@ const fetchData = async ({ result, page }) => {
     const count = data.count;
     const stores = data.stores.map((store) => {
       return {
-        url: `https://sllr.co/${store.name}`,
+        url: `https://sllr.co/${store?.storeInfo?.storeLink || ""}`,
         lastModified: new Date(),
+        priority: 1.0,
       };
     });
     result.push(...stores);
